ffa81cbd06d242556527bfe113719a7b09618625
Olga Sandu <sandu.helga@gmail.com>
2022-02-16 16:55:31 +0100

    Add files via upload

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,18 @@
+import react, { Component } from "react";
+
+class ErrorBoundary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false 
+        }
+    }
+    render() {
+        if (this.state.hasError) {
+            return <h1>Oooops.That is not good</h1>
+        } 
+        return this.props.children
+    }
+}
+
+export default ErrorBoundary;
